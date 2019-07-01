21f48fa0d8c4969ecb92868495c9bd77cba10c73
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-07-01 15:24:46 -0500

    Added DisplayInventory() method in player.

---
+++ Items.cs
@@ -0,0 +1,27 @@
+namespace lemonadeStandGame
+{
+    public class Items
+    {
+        // member variables
+        public string name {get; set;}
+        public double price {get; set;}
+        public int amount {get; set;}
+
+        // constructor
+        public Items()
+        {
+            
+        }
+
+        // member methods
+        public void AddItem()
+        {
+
+        }
+
+        public void RemoveItem()
+        {
+
+        }
+    }
+}
--- Player.cs
+++ Player.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace lemonadeStandGame
 {
@@ -7,14 +8,22 @@ namespace lemonadeStandGame
         // member variables
         public string name {get; set;}
         public double balance {get; set;}
-        public Inventory inventory;
-
+        public List<Items> itemList;
+        public Items paperCups {get; set;}
+        public Items lemons {get; set;}
+        public Items cupsOfSugar {get; set;}
+        public Items iceCubes {get; set;}
 
         // constructor
         public Player(){
-            inventory = new Inventory();
+            paperCups = new Items();
+            lemons = new Items();
+            cupsOfSugar = new Items();
+            iceCubes = new Items();
+            AddItems();
             balance = 20;
             SetName();
+            BuyItems();
         }
 
         // member methods
@@ -22,12 +31,42 @@ namespace lemonadeStandGame
         {
             Console.Write("\nPlease enter player name: ");
             name = Console.ReadLine();
-            Console.WriteLine(name);
         }
 
-        public void BuyItems(string itemName, int amount)
+        public void BuyItems()
         {
-            
+
+        }
+
+        public void CreateRecipe()
+        {
+
+        }
+
+        public void DisplayInventory()
+        {
+            Console.WriteLine($"Paper Cups: {paperCups.amount}, Lemons: {lemons.amount}, Cups of Sugar: {cupsOfSugar.amount}, Ice Cubes: {iceCubes.amount}");
+        }
+
+        public void AddItems()
+        {
+            itemList = new List<Items>();
+            itemList.Add(paperCups);
+            itemList.Add(lemons);
+            itemList.Add(cupsOfSugar);
+            itemList.Add(iceCubes);
+            paperCups.name = "papercups";
+            paperCups.amount = 0;
+            paperCups.price = 0.02;
+            lemons.name = "lemons";
+            lemons.amount = 0;
+            lemons.price = 0.04;
+            cupsOfSugar.name = "cupsofsugar";
+            cupsOfSugar.amount = 0;
+            cupsOfSugar.price = 0.04;
+            iceCubes.name = "icecubes";
+            iceCubes.amount = 0;
+            iceCubes.price = 0.01;
         }
     }
 }
